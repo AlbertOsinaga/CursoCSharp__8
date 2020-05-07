9b96cf710502cca3f5fbf4df3f5c6d5dc5ac65c2
Alberto Osinaga <luis.osinaga@gmail.com>
2020-05-07 19:55:34 -0400

    V 2020 05 07 PPT CQR 201 slides

---
+++ Consola/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using static System.Console;
+using System.IO;
+
+namespace Consola
+{
+    class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                uint num = uint.MaxValue;
+                WriteLine(num++);
+                WriteLine(num);
+            }
+            catch (NullReferenceException)
+            {
+                WriteLine("Error: uso de objeto no inicializado! ");
+            }
+            catch (DivideByZeroException)
+            {
+                WriteLine("Error de division por cero! ");
+            }
+            catch (Exception ex)
+            {
+                WriteLine($"Error: tipo - {ex.GetType().Name} " +
+                        $" ; mensaje - {ex.Message} ");
+            }
+            // catch {
+            // WriteLine("Un error inesperado sucedió!");   }
+        }
+
+
+
+    }
+}
